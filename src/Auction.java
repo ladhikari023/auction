237ba42404dcc9561470b2f97b9ad7420e5c2dc1
Brian Kimbrell <bkimbrell@unm.edu>
2021-12-02 10:45:48 -0700

    first commit

--- src/Auction.java
+++ src/Auction.java
@@ -4,13 +4,31 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+// The Auction Class will contain the runnable part of operating the auction
+// generating an Auction will automatically generate an AuctionHouse and connect
+// it to the Bank.
 public class Auction implements Runnable
 {
-    Socket bank;
-    Socket auctionHouse;
+    private final Socket bank;
+    private final Socket auctionHouse;
+    private final PrintWriter out;
+    private final BufferedReader in;
 
-    public Auction(Socket bank, Socket auctionHouse)
+    public Auction(Socket bank)
+    {
+         this.bank = bank;
+    }
+
+    // Print the list of items up for auction
+    public void printItemList()
     {
 
     }
+
+    @Override
+    public void run()
+    {
+
+    }
+
 }
