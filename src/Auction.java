0643667d805413e458f5dc4f05844dc0873d7f88
Brian Kimbrell <bkimbrell@unm.edu>
2021-12-02 10:11:41 -0700

    First commit

---
+++ src/Auction.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Auction implements Runnable
+{
+    Socket bank;
+    Socket auctionHouse;
+
+    public Auction(Socket bank, Socket auctionHouse)
+    {
+
+    }
+}
