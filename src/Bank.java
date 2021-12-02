5769cdb511c1d09aebed922cceee2da2509d82c2
Brian Kimbrell <bkimbrell@unm.edu>
2021-12-02 10:54:33 -0700

    second

--- src/Bank.java
+++ src/Bank.java
@@ -1,5 +1,14 @@
-package Bank;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class Bank {
-    // private fields
+public class Bank
+{
+    public void main(String[] args)
+    {
+        int port = Integer.parseInt(args[0]);
+
+        ServerSocket bankSever = new ServerSocket(port);
+    }
+
 }
